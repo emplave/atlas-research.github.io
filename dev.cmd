61afd5d1134e4dd6753baeab31a901fa19596472
hamaad <117494079+emplave@users.noreply.github.com>
2026-07-22 22:56:39 -0700

    Delete dev.cmd

--- dev.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Dev-server wrapper: guarantees Node on PATH regardless of parent env
-rem (needed because shells that predate the Node install lack it)
-set "PATH=C:\Program Files\nodejs;%PATH%"
-cd /d "%~dp0"
-npm run dev
